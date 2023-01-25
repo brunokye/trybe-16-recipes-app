615ff100a01743c6af7d7d8817b77c62104f8715
Bruno Ferreira <contatobrunokye@gmail.com>
2023-01-25 20:30:51 -0300

    feat: requisito 09

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title, searchEnabled }) {
+  const [bar, setBar] = useState(false);
+
+  const searchBar = () => {
+    if (bar === false) setBar(true);
+    if (bar === true) setBar(false);
+  };
+
   return (
     <div>
       <span data-testid="page-title">{ title }</span>
 
-      { searchEnabled ? (
-        <img
-          data-testid="search-top-btn"
-          src={ searchIcon }
-          alt="search"
+      { bar ? (
+        <input
+          data-testid="search-input"
+          type="text"
         />)
         : ''}
 
-      <Link to="/profile">
+      { searchEnabled ? (
+        <button
+          type="button"
+          onClick={ searchBar }
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="search"
+          />
+        </button>)
+        : ''}
+
+      <Link to="profile">
         <img
           data-testid="profile-top-btn"
           src={ profileIcon }
           alt="profile"
         />
       </Link>
+
     </div>
   );
 }
