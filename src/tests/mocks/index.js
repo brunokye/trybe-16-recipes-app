4739fe83ea2ebdea5dda05abbbf09afd44a9c392
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-01-26 20:35:37 -0300

    test: adiciona testes da página principal de receitas

--- src/tests/mocks/index.js
+++ src/tests/mocks/index.js
@@ -1,2 +1,6 @@
 export * as emptyDrinkResponse from './emptyDrinksResponse.json';
 export * as emptyFoodResponse from './emptyFoodResponse.json';
+export * as foodResponse from './foodResponse.json';
+export * as drinksResponse from './drinksResponse.json';
+export * as foodCategoriesResponse from './foodCategoriesResponse.json';
+export * as drinksCategoriesResponse from './drinksCategoriesResponse.json';
