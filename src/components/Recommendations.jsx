52bf1c923a53d24103b933965868bf05c3fbf51a
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-01-30 22:16:10 -0300

    fix: corrige visualização de carousel exibindo os dois primeiros após o indice atual

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Recommendations({ recipes }) {
-  // const [notShow, setNotShow] = useState(true);
-  const SIX = 6;
+export default function Recommendations({ recipes, currentIndex }) {
+  const hideAfter = currentIndex + 1;
+  const maxItems = 6;
 
   return (
     <>
-      {recipes.slice(0, SIX).map((recipe, index) => (
+      {recipes.slice(0, maxItems).map((recipe, index) => (
         <div
           key={ recipe.strMeal || recipe.strDrink }
           className="image-container"
         >
           <img
             data-testid={ `${index}-recommendation-card` }
-            className="image-size"
+            className={ `image-size ${hideAfter < index ? 'hidden' : ''}` }
             src={ recipe.strMealThumb || recipe.strDrinkThumb }
             alt={ recipe.strMeal || recipe.strDrink }
           />
